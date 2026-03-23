5452902d8db0c4f4ac671670d17c191d3de7ffdd
floflodev <6254fxppjh@privaterelay.appleid.com>
2026-03-23 23:20:53 +0100

    Update Activité_2.py
    Added functions for substitution cipher encoding and decoding.

--- Activité_2.py
+++ Activité_2.py
@@ -29,11 +29,41 @@ print(resultat) # Affiche y
 # Programmer une fonction qui renvoie une phrase codée par substitution 
 
 def chiffre_substitution_phrase(phrase) :
+    # Initialise une nouvelle phrase 
     nouvelle_phrase = ""
     for car in phrase :
+        # Utilise la fonction chiffre_substitution_caractere 
         nouvelle_lettre_phrase = chiffre_substitution_caractere(car)
+        # Défini la nouvelle phrase en ajoutant les caracteres au fur et a mesure 
         nouvelle_phrase += nouvelle_lettre_phrase
     return nouvelle_phrase
-    
-#resultat2 = chiffre_substitution_phrase("PAS DE POTION POUR OBELIX !")
-#print(resultat2)
+# Exemple avec la phrase "PAS DE POTION POUR OBELIX !"
+resultat = chiffre_substitution_phrase("PAS DE POTION POUR OBELIX !")
+print(resultat) # Affiche uyi od ubpgbn ubhq bkdxgs !
+
+# 3)
+# Objectif : 
+# Programmer une fonction qui va déchiffrer une phrase codée par substitution
+
+# On créer une premiere fonction servant a inverser notre mélange et l'alphabet
+# A l'inverse de notre fonction chiffre_substitution_caractere qui passait de l'alphabet au mélange
+# celle ci passe du mélange a l'alphabet 
+def dechiffre_substitution_caractere(car) :
+    if car not in Melange :
+            return car
+    i = Melange.index(car)
+    j = Alphabet[i]
+    return j 
+
+# Cette fonction est construite de la meme maniere que la fonction chiffre_substitution_phrase
+# Nous faisons cependant appel a la la fonction dechiffre_substitution_caractere 
+def dechiffre_substitution_phrase(phrase) : 
+    nouvelle_phrase = ""
+    for car in phrase :
+        nouvelle_lettre_phrase = dechiffre_substitution_caractere(car)
+        nouvelle_phrase += nouvelle_lettre_phrase
+    return nouvelle_phrase 
+# Exemple avec la phrase "uyi od ubpgbn ubhq bkdxgs !"
+resultat = dechiffre_substitution_phrase("uyi od ubpgbn ubhq bkdxgs !")
+print(resultat) # Affiche PAS DE POTION POUR OBELIX !
+
